377d29967ba67544d2123712a7aae939cb63e164
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-04-28 18:48:15 +0500

    Update _footer.php

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -21,7 +21,7 @@ use yii\helpers\Url;
                 <div class="mb_menu_list2">
                     <ul>
                         <li>
-                            <a href="https://tgfu.uz/">
+                            <a href="https://arbu.uz/">
                                 <span><?= Yii::t("app" , "a22") ?> <b>ABU RAYHON BERUNIY UNIVERSITETI</b></span>
                             </a>
                         </li>
