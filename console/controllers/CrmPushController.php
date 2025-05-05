62c1f32930cd06efcafd4042e13b7308e1f4e7d3
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-05 12:21:30 +0500

    Update CrmPushController.php

--- console/controllers/CrmPushController.php
+++ console/controllers/CrmPushController.php
@@ -36,7 +36,7 @@ class CrmPushController extends Controller
         $transaction = Yii::$app->db->beginTransaction();
 
         $query = CrmPush::find()
-            ->where(['status' => 0, 'is_deleted' => 0, 'student_id' => 603])
+            ->where(['status' => 0, 'student_id' => 603])
             ->andWhere([
                 'or',
                 ['and', ['type' => 1], ['lead_id' => null]],  // type 1 uchun lead_id null bo'lishi kerak
@@ -84,7 +84,7 @@ class CrmPushController extends Controller
             $phoneNumber = preg_replace('/[^\d+]/', '', $student->username);
             $leadName = $phoneNumber;
             $message = '';
-            $tags = ['zarmed'];
+            $tags = ['arbu'];
             $pipelineId = AmoCrmClient::DEFAULT_PIPELINE_ID;
             $statusId = $model->lead_status;
             $leadPrice = 0;
