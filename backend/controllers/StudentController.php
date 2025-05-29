6526958be5dd5cf2b042bbfedf46de0491648666
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-29 14:18:54 +0500

    Update StudentController.php

--- backend/controllers/StudentController.php
+++ backend/controllers/StudentController.php
@@ -732,7 +732,7 @@ class StudentController extends Controller
     protected function findModel($id)
     {
         if (($model = Student::findOne(['id' => $id])) !== null) {
-            $user = $model->student->user;
+            $user = $model->user;
             if ($user->status != 0) {
                 return $model;
             }
