b0a090945deaa8749e1b5a47084d13589bfbe78b
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-31 20:06:02 +0500

    Update main.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,7 +44,6 @@ AppAsset::register($this);
     <?= $this->render('_footer') ; ?>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <?php $this->endBody() ?>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
