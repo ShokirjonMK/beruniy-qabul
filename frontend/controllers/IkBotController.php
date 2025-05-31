df2830b6be7385c645eaefc20a3e681785c2cd60
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-31 20:28:52 +0500

    Update IkBotController.php

--- frontend/controllers/IkBotController.php
+++ frontend/controllers/IkBotController.php
@@ -27,34 +27,4 @@ class IkBotController extends Controller
         Bot::telegram($telegram);
     }
 
-    public function actionCons2()
-    {
-        // Xamkor yozib ketiladi, eslab qolish uchun;
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        $telegram = Yii::$app->telegram2;
-
-        $data = Yii::$app->request->getRawBody();
-        $telegramArray = json_decode($data, true);
-
-        $telegram_id = $telegramArray['message']['chat']['id'] ?? null;
-
-        return $telegram->sendMessage([
-            'chat_id' => $telegram_id,
-            'text' => "Ro'yhatdan o'tish uchun quyidagi tugmani bosing:",
-            'reply_markup' => json_encode([
-                'inline_keyboard' => [
-                    [
-                        [
-                            'text' => "Ro'yhatdan o'tish",
-                            'web_app' => [
-                                'url' => 'https://arbu-edu.uz'
-                            ]
-                        ]
-                    ]
-                ]
-            ])
-        ]);
-    }
-
-
 }
