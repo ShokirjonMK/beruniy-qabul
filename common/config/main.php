d5ea5f294d0b1abb473f89228567c78e69659d2d
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-31 20:53:42 +0500

    1

--- common/config/main.php
+++ common/config/main.php
@@ -17,10 +17,6 @@ return [
             'class' => 'aki\telegram\Telegram',
             'botToken' => '8132126478:AAG0rC9gkBCokIXqiSUsDl2uMxPFXp_Xtps',
         ],
-        'telegram2' => [
-            'class' => 'aki\telegram\Telegram',
-            'botToken' => '8173823388:AAELfo6VRvhT2mUZ50jMfmwg4WBk2GpvQCs',
-        ],
         'ikAmoCrm' => [
             'class' => 'common\components\AmoCrmClient',
         ],
--- frontend/controllers/IkBotController.php
+++ frontend/controllers/IkBotController.php
@@ -27,4 +27,5 @@ class IkBotController extends Controller
         Bot::telegram($telegram);
     }
 
+
 }
