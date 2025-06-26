353c1b5ae3852eacd759a30d371e13cc990affe3
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-26 19:09:44 +0500

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -15,7 +15,8 @@ return [
         ],
         'telegram' => [
             'class' => 'aki\telegram\Telegram',
-            'botToken' => '7693608040:AAE0RCzU4V96DNNJ7jgvDn72md5-Ylj9N_I',
+            'botToken' => '7789278923:AAFUm97oazOnnGMg6rvf6iASvlMAOAklxQw',
+//            'botToken' => '7693608040:AAE0RCzU4V96DNNJ7jgvDn72md5-Ylj9N_I',
         ],
         'ikAmoCrm' => [
             'class' => 'common\components\AmoCrmClient',
