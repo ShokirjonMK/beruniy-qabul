7abce9528f73e27f5dfff087f87b0f138de9bdba
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-26 15:57:58 +0500

    Update MenuController.php

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -21,11 +21,6 @@ class MenuController extends Controller
 
     public function actionIndex()
     {
-        CrmPush::updateAll(
-            ['status' => 0],
-            ['and', ['lead_id' => 25904313], ['<>', 'type', 1]]
-        );
-        dd(232323);
         $searchModel = new MenuSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
