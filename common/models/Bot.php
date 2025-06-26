4209f55c41e8b146b8a0c384c77088be7db42a4a
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-26 14:39:39 +0500

    Update Bot.php

--- common/models/Bot.php
+++ common/models/Bot.php
@@ -463,12 +463,25 @@ class Bot extends Model
 
                             $fileUrl = "https://arbu-edu.uz/backend/web/uploads/contract/".$fileName;
 
-                            $tt =  $telegram->sendDocument([
-                                'chat_id' => $gram->telegram_id,
-                                'document' => $fileUrl,
-                                'caption' => $student->fullName . "__shartnoma",
-                            ]);
-                            var_dump($tt);
+                            $chat_id = $gram->telegram_id;
+                            $url  = $fileUrl;
+                            $caption=  "Test Document";
+                            $keyboards = null;
+                            $curl = curl_init();
+                            curl_setopt_array($curl, array(
+                                CURLOPT_URL => 'https://api.telegram.org/bot'.$telegram->botToken.'/sendDocument?chat_id=' . $chat_id . '&document=' . $url . '&caption=' . $caption . '&reply_markup=' . $keyboards,
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_ENCODING => '',
+                                CURLOPT_MAXREDIRS => 10,
+                                CURLOPT_TIMEOUT => 0,
+                                CURLOPT_FOLLOWLOCATION => true,
+                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                CURLOPT_CUSTOMREQUEST => 'GET',
+                            ));
+                            $response = curl_exec($curl);
+                            curl_close($curl);
+                            $data = (json_decode($response));
+                            var_dump($data);
                         }
                     }
                 }
