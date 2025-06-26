827adc8fd9775b5211a4dc572b81e9a0a4bbe077
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-26 16:53:54 +0500

    1

--- common/models/Bot.php
+++ common/models/Bot.php
@@ -476,12 +476,12 @@ class Bot extends Model
                             curl_setopt_array($curl, array(
                                 CURLOPT_URL => 'https://api.telegram.org/bot'.$telegram->botToken.'/sendDocument?chat_id=' . $chat_id . '&document=' . $url . '&caption=' . $caption,
                                 CURLOPT_RETURNTRANSFER => true,
-                                CURLOPT_ENCODING => '',
-                                CURLOPT_MAXREDIRS => 10,
-                                CURLOPT_TIMEOUT => 0,
-                                CURLOPT_FOLLOWLOCATION => true,
-                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                                CURLOPT_CUSTOMREQUEST => 'GET',
+                                CURLOPT_POST => true,
+                                CURLOPT_POSTFIELDS => [
+                                    'chat_id' => $chat_id,
+                                    'document' => $url,
+                                    'caption' => $caption
+                                ],
                             ));
                             $response = curl_exec($curl);
                             curl_close($curl);
